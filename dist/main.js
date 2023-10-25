09543cd09e15e11215fd8cd1d717019fd87390e9
Ednaira Viana <ednairaviana.evs@gmail.com>
2023-10-25 17:09:36 -0300

    Create square component

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";document.querySelector("#first");const e=document.querySelector("#second");!function(){for(let t=0;t<100;t++){const t=document.createElement("div");t.classList.add("square"),e.insertAdjacentElement("beforeend",t)}}()})();
