49131a6f671a6a5f8216d7fe419af129d7ee142e
Ednaira Viana <ednairaviana.evs@gmail.com>
2023-10-25 18:02:42 -0300

    Create attackAnimation

--- src/dom_methods/dom_game_board.js
+++ src/dom_methods/dom_game_board.js
@@ -5,11 +5,36 @@ function renderSquares() {
   createSquares(domObj.board.second);
 }
 
+function attackAnimation(comp) {
+  const mainColor = "#00229c";
+  const bcgColor = "#151515";
+
+  comp.style.pointerEvents = "none";
+  comp.style.backgroundColor = "#00229c";
+
+  setTimeout(setBcgColor, 300);
+  setTimeout(setMainColor, 600);
+  setTimeout(setBcgColor, 900);
+  setTimeout(setMainColor, 1200);
+
+  function setMainColor() {
+    comp.style.backgroundColor = mainColor;
+  }
+
+  function setBcgColor() {
+    comp.style.backgroundColor = bcgColor;
+  }
+}
+
 function createSquares(parent) {
   for (let i = 0; i < 100; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
     parent.insertAdjacentElement("beforeend", square);
+
+    square.addEventListener("click", () => {
+      attackAnimation(square);
+    });
   }
 }
 
